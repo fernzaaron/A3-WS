6d71f17235b92b6c4d9dbd963b30eb7bab03682b
Your Name <you@example.com>
2025-11-19 20:08:02 -0500

    table with db + routers

--- config/app.js
+++ config/app.js
@@ -11,6 +11,7 @@ dotenv.config({ path: './config/.env'});
 
 var indexRouter = require('../routes/index');
 var usersRouter = require('../routes/users');
+let taskRouter = require('../routes/task');
 
 var app = express();
 // connect to MongoDB
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../node_modules')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/tasks', taskRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/task.js
@@ -0,0 +1,19 @@
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+/* CRUD operations for tasks */
+let Task = require('../models/tasks');
+
+/* GET all tasks */
+router.get('/', async (req, res, next) => {
+        try {
+                const TaskList = await Task.find();
+                // provide both `tasks` and `TaskList` so templates using either name work
+                res.render('task', { title: 'Tasks', tasks: TaskList, TaskList: TaskList });
+        } catch (err) {
+                console.error(err);
+                return res.status(500).render('error', { title: 'Error', message: 'Failed to fetch tasks' });
+        }
+});
+
+module.exports = router;
